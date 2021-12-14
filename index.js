0dcd5cddd7ddb3e8037cf01ce7280244c5f3453a
Pedro A. González <pgonzalez@post.com>
2021-12-13 20:22:33 -0500

    Estructura básica del servidor

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola!');
+// REQUIREMENTS
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// SERVER SETTINGS + MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ROUTES/ENDPOINTS
+app.get('/', (req, res) => {
+  return res.send('Hola!');
+});
+
+// LISTENER
+app.listen(PORT, () =>
+  console.log('Servidor corriendo en http://localhost:' + PORT)
+);
