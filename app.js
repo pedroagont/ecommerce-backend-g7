9932dd273dc1f24ea3b334c993e1848f5457deb8
Pedro A. González <pgonzalez@post.com>
2021-12-16 21:29:07 -0500

    Fix error CORS (instalar paquete cors y agregar uso de middleware)

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 const middlewares = require('./middlewares');
 require('./db');
 
 // SERVER SETTINGS + MIDDLEWARES
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
